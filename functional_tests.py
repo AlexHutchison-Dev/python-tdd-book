fd8b13c654dc83e43d438d7f8370347037776ba7
Alex Hutchison <a.hutchison@live.co.uk>
2022-02-03 12:58:50 -0800

    refactor functional tests to remove repetition

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app.
         # She goes to check out its homepage
@@ -34,13 +39,9 @@ class NewVisitorTest(unittest.TestCase):
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id("id_list-table")
+        table = self.browser.find_element_by_id("id_list_table")
         rows = table.find_elements_by_tag_name("tr")
-        self.assertIn(
-            "1: Buy peacock feathers",
-            [row.text for row in rows],
-            f"New item did not appear in table. Contents were: \n {table.text}",
-        )
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
 
         # There is still a text box inviting her to add another item. She enters
         # "Use peacock feathers to make a fly" (Edith is very Methodical)
@@ -51,18 +52,11 @@ class NewVisitorTest(unittest.TestCase):
 
         # The page updates again, now showing both items on her list
 
-        table = self.browser.find_element_by_id("id_list-table")
+        table = self.browser.find_element_by_id("id_list_table")
         rows = self.browser.find_elements_by_tag_name("tr")
-        self.assertIn(
-            "1: Buy peacock feathers",
-            [row.text for row in rows],
-            f"New item did not appear in table. Contents were: \n {table.text}",
-        )
-        self.assertIn(
-            "2: Use peacock feathers to make a fly",
-            [row.text for row in rows],
-            f"New item did not appear in table. Contents were: \n {table.text}",
-        )
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make fly")
+
         # Edith wonders weather the site will remember her list. Then she notices
         # that the site has generated a unique URL for her -- there is some
         # explanitory text to that effect
