3f7c76236d2f38e4c12d9d9245b3824948e02298
Alex Hutchison <a.hutchison@live.co.uk>
2022-02-03 19:26:31 -0800

    Redirect after POST, and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -37,7 +37,7 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item on the to-do list
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(0.5)
 
         table = self.browser.find_element_by_id("id_list_table")
         rows = table.find_elements_by_tag_name("tr")
@@ -48,7 +48,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id("id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(0.5)
 
         # The page updates again, now showing both items on her list
 
---
+++ scratchpad.md
@@ -0,0 +1,12 @@
+# Scratchpad
+## Project notes and todo's
+
+### ToDo
+
+* Display multiple items in the table
+* Suport multiple lists
+
+### Completed
+
+* Don't save a blank item for every request
+* Code smell: POST test is getting long
