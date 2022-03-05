520c079609a5bf0b1e2a29a5799e9a0e395cfd6a
Alex Hutchison <a.hutchison@live.co.uk>
2022-03-04 17:11:21 -0800

    hack to ALLOWED_HOSTS to be *

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "81z5wzgx&(1z&_!(w^h1pe$@8bwy5lrzug%*th!ib4_qehpbf8"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -119,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
